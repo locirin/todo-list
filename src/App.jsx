6aedb0be1bca34625ba51ea5c0d4a35096e7bbea
Irina Lo <196596248+locirin@users.noreply.github.com>
2025-08-12 20:55:29 -0700

    Week 5: conditional message, complete todo, controlled form, disable add button

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,22 @@ function App() {
   const [todoList, setTodoList] = useState([]);
 
   function addTodo(title) {
-    const newTodo = { id: Date.now(), title };
+    const newTodo = { id: Date.now(), title, isCompleted: false };
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: true } : todo
+    );
+    setTodoList(updatedTodos);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,5 +1,17 @@
-function TodoListItem({ todo }) {
-  return <li>{todo.title}</li>;
+function TodoListItem({ todo, onCompleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.isCompleted}
+          onChange={() => onCompleteTodo(todo.id)}
+        />{' '}
+        {''}
+        {todo.title}
+      </label>
+    </li>
+  );
 }
 
 export default TodoListItem;
